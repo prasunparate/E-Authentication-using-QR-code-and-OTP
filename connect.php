b5c0eac4ee7b9cbb64565d9c49cfe16924651487
Prasun Kumar Parate <40351757+prasunparate@users.noreply.github.com>
2020-03-07 08:37:47 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,70 @@
+<?php
+$username = filter_input(INPUT_POST, 'username');
+$password = filter_input(INPUT_POST, 'password');
+$email = filter_input(INPUT_POST,'email');
+$mobile = filter_input(INPUT_POST,'mobile');
+if (!empty($username)){
+if (!empty($password)){
+if (!empty($email)){
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "prasun";
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+if (mysqli_connect_error()){
+die('Connect Error ('. mysqli_connect_errno() .') '
+. mysqli_connect_error());
+}
+else{
+   
+    if($email != "")
+    { $res = mysqli_query($conn, "SELECT * from `simpleregistration` where email='".$email."'");
+      $num_rows= mysqli_num_rows($res);
+        if($num_rows>0)
+        {echo "Email Exists";
+          die();
+        }
+    }
+    if($mobile !="")
+    {
+        $res = mysqli_query($conn,"SELECT * FROM `simpleregistration` where mobile='".$mobile."'");
+        $num_rows= mysqli_num_rows($res);
+        if($num_rows>1)
+        {
+            echo "You cannot register the same mobile number thrice";
+            die();
+        }
+    }
+   
+    
+    $sql = "INSERT INTO simpleregistration (username, password, email, mobile)
+values ('$username','$password','$email','$mobile')";
+  
+if ($conn->query($sql)){
+echo "New record is inserted sucessfully";
+}
+else{
+echo "Error: ". $sql ."
+". $conn->error;
+}
+$conn->close();
+}
+ 
+
+}
+else {
+     echo "Email should not be empty";
+    die();
+      }
+}
+else{
+echo "Password should not be empty";
+die();
+}
+}
+else{
+echo "Username should not be empty";
+die();
+}
+?>
