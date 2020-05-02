e03566485cb16091c162df639ca14c9a4193bae5
Prasun Kumar Parate <40351757+prasunparate@users.noreply.github.com>
2020-05-02 21:57:31 +0530

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,48 @@
+<?php
+$roll = $_POST['roll'];
+$name = $_POST['name'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+$country = $_POST['country'];
+$lan = $_POST['lan'];
+$gen = $_POST['gen'];
+$add = $_POST['add'];
+$date = $_POST['date'];
+$phonecode = $_POST['phonecode'];
+if (!empty($roll) || !empty($name) || !empty($password) || !empty($email) || !empty($country) || !empty($country) ||
+ !empty($lan) || !empty($gen) || !empty($add) || !empty($date) || !empty($phonecode))
+{ $host = "localhost";
+  $dbUsername = "root";
+  $dbPassword = "";
+  $dbname = "prasun";
+  // create connection
+   $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+   if (mysqli_connect_error()) 
+  { die('Connection Error ('.mysqli_connection_error().')'.mysqli_connect_error());
+   } else { $SELECT = "SELECT email From registration Where email = ? Limit 1";
+            $INSERT = "INSERT into registration (roll,name,password,email,country,language,gender,address,dob,mob) values (?,?,?,?,?,?,?,?,?,?)";
+      //prepare statement 
+     $stmt = $conn->prepare($SELECT);
+     $stmt->bind_param("s",$email);
+     $stmt->execute();
+     $stmt->bind_result($email);
+     $stmt->store_result();
+     $rnum = $stmt->num_rows;
+ if ($rnum==0)
+  { $stmt->close();
+     $stmt = $conn->prepare($INSERT);
+     $stmt->bind_param("ssssii", $roll,$name,$password,$email,$country,$language,$gender,$address,$dob,$mob);
+    $stmt->execute();
+    echo "New Record inserted successfully";
+  } else  { 
+           echo "Someone already registered usingthis email."
+           }
+    $stmt->close();
+    $conn->close();
+ 
+}
+else { 
+       echo "All field are required";
+      die();
+    }
+?>
---
+++ otp.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+if(isset($_POST['save']))
+{
+$rno=$_SESSION['otp'];
+$urno=$_POST['otpvalue'];
+if(!strcmp($rno,$urno))
+{
+$name=$_SESSION['name'];
+$email=$_SESSION['email'];
+$phone=$_SESSION['phone'];
+//For admin if he want to know who is register
+$to="example@gmail.com";
+$subject = "Thank you!";
+$txt = "Some one show your demo Email id: ".$email." Mobile number : ".$phone."";
+$headers = "From: studentstutorial@gmail.com" . "\r\n" .
+"CC: divyasundarsahu@gmail.com";
+mail($to,$subject,$txt,$headers);
+echo "<p>Thank you for show our Demo.</p>";
+//For admin if he want to know who is register
+}
+else{
+echo "<p>Invalid OTP</p>";
+}
+}
+//resend OTP
+if(isset($_POST['resend']))
+{
+$message="<p class='w3-text-green'>Sucessfully send OTP to your mail.</p>";
+$rno=$_SESSION['otp'];
+$to=$_SESSION['email'];
+$subject = "OTP";
+$txt = "OTP: ".$rno."";
+$headers = "From: otp@studentstutorial.com" . "\r\n" .
+"CC: divyasundarsahu@gmail.com";
+mail($to,$subject,$txt,$headers);
+$message="<p class='w3-text-green w3-center'><b>Sucessfully resend OTP to your mail.</b></p>";
+}
+?>
